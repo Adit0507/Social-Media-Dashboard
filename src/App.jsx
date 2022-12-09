776201d04dd6353fd7f547549ad175cba15baaf8
Adit0507 <adi05072002@gmail.com>
2022-12-09 21:30:14 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl text-align:center font-bold underline">HIQEE</h1>      
+    </div>
+  )
+}
+
+export default App
