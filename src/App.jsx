f5cbf4efc80292192a3b096cebc52d81873b716e
Adit0507 <adi05072002@gmail.com>
2022-12-10 16:29:34 +0530

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import Header from "./components/Header"
-import Followers from "./components/Followers"
-import Overview from "./components/Overview"
+import Header from "./components/Header";
+import Followers from "./components/Followers";
+import Overview from "./components/Overview";
 
 function App() {
   return (
     <>
-      <section>
+      <section className="p-8 dark:bg-slate-900 h-full lg:h-screen">
+        <div
+          className="bg-slate-100 dark:bg-slate-800 h-52 w-full absolute top-0 left-0 rounded-b-3xl"
+          style={{ zIndex: 0, }}
+        ></div>
         <Header />
         <Followers />
         <Overview />
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
